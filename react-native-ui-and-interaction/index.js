eeaa50425fc1f85f81a2743f9f6ea72647739972
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2026-02-11 21:05:28 +0800

    Done with style section

---
+++ react-native-ui-and-interaction/index.js
@@ -0,0 +1,11 @@
+import { registerRootComponent } from 'expo';
+
+import App from './App';
+import Styles from './styles/Styles';
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+// registerRootComponent(App);
+
+registerRootComponent(Styles)
