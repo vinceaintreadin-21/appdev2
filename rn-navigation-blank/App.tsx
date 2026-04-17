93f6cf80256618565801f328e320e29f6a658db6
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2026-04-17 11:15:53 +0800

    done with go back

--- rn-navigation-blank/App.tsx
+++ rn-navigation-blank/App.tsx
@@ -28,7 +28,7 @@ function DetailsScreen() {
       <Button onPress={() => navigation.push('Details')}>
         Go to Details... again
       </Button>
-
+      <Button onPress={() => navigation.goBack()}>Go back</Button>
     </View>
   );
 }
