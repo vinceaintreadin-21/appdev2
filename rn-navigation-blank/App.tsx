16c095fe683d0b570b598355578b68b046de5480
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2026-04-17 10:47:54 +0800

    Done specifying options

--- rn-navigation-blank/App.tsx
+++ rn-navigation-blank/App.tsx
@@ -21,8 +21,17 @@ function DetailsScreen() {
 
 const RootStack = createNativeStackNavigator({
   initialRouteName: 'Home',
+  screenOptions: {
+    headerStyle: { backgroundColor: 'tomato' },
+
+  },
   screens: {
-    Home: HomeScreen,
+    Home: {
+      screen:  HomeScreen,
+      options: {
+        title: 'Overview'
+      }
+    }
   },
   Details: DetailsScreen,
 });
