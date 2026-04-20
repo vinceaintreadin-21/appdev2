24c21387d597c9ff624b47a666560d3692ea1345
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2026-04-20 09:50:38 +0800

    Imported and configured TypeScript in App file

--- react-native-navigation/types.ts
+++ react-native-navigation/types.ts
@@ -1,20 +1,31 @@
-import type { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import type { RouteProp } from "@react-navigation/native";
+import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import type { RouteProp } from '@react-navigation/native';
 
-//Root 
-type RootStackParamList = {
-    Home: undefined;
-    Details: {
-        itemId: number;
-        otherParam?: string;
-    }
-}
+//Root
+export type RootStackParamList = {
+  Home: undefined;
+  Details: {
+    itemId: number;
+    otherParam?: string;
+  };
+};
 
 //Home
-export type HomeScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, 'Home'>
+export type HomeScreenNavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  'Home'
+>;
 
 //Details
-type DetailsScreenRouteProp = RouteProp<RootStackParamList, 'Details'>
+export type DetailsScreenNavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  'Details'
+>;
+
+export type DetailsScreenRouteProp = RouteProp<
+  RootStackParamList,
+  'Details'
+>;
 
 export type DetailsProps = {
   route: DetailsScreenRouteProp;
