1efefbabe6ec7de18acadd8cebb0d68e940be831
vinceaintreadin-21 <vincentleeduriga@student.laverdad.edu.ph>
2026-04-24 06:32:44 +0800

    Done with copy App.jsx code

---
+++ react-native-todo-convex/App.tsx
@@ -0,0 +1,15 @@
+
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import TodoScreen from "./screens/TodoScreen";
+
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+  unsavedChangesWarning: false,
+});
+
+export default function App() {
+  return (
+    <ConvexProvider client={convex}>
+      <TodoScreen />
+    </ConvexProvider>
+  );
+}
